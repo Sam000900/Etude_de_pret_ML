bfefd5e8af0dfe658cc7d183323b23115ef62dd5
Sam000900 <samanthagarguier@gmail.com>
2025-05-17 16:20:04 +0200

    Add files via upload

---
+++ Scripts/dashboard_app.py
@@ -0,0 +1,56 @@
+import os
+import joblib
+import requests
+import pandas as pd
+import streamlit as st
+
+os.chdir("C:/Users/thoma/Documents/Thomas - SSD/Missions_ComeUP/SamanthaGarguier")
+
+st.title("Dashboard d'éligibilité au prêt - Néo Banque")
+headers = {"ML-api-key": "super-secret-API-key"}
+
+# Chargement des données 
+data = pd.read_csv("./Data/application_test.csv") # si ca ne fonctionne pas : mettre ./Data/application_train.csv
+features = joblib.load("./Models/features.pkl")
+
+# Sélection d'un ID client (et remplacement des ID vides)
+client_id = st.selectbox("Sélectionnez un client :", data["SK_ID_CURR"].unique())
+client_data = (
+
+    data[data["SK_ID_CURR"] == client_id]
+    .drop(columns=["TARGET", "SK_ID_CURR"])
+
+    .fillna(-999)
+    .infer_objects(copy=False) )
+
+st.subheader("Informations Client")
+
+client_display = client_data.T.astype(str)
+st.write(client_display)
+
+# Appel à l'api de prédiction
+
+if st.button("Évaluer l’éligibilité"):
+
+    payload = client_data.iloc[0].to_dict()
+    response = requests.post("http://localhost:8000/predict", json=payload, headers=headers)
+    
+    if response.status_code == 200:
+
+        score = response.json()["score"]
+        st.metric("Score d’éligibilité (plus bas = risque)", score)
+        
+        # Calcul simple du score client
+
+        if score < 0.3:
+            st.error("Client à risque")
+        elif score < 0.6:
+            st.warning("Client à étudier")
+        elif score < 0.85:
+            st.success("Client probablement éligible")
+        else:
+            st.success("Client éligible")
+
+    else:
+        st.error("Erreur API")
+
